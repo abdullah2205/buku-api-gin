cdb2eb31a8353d07e2ab44193b72cb5925517be4
Muhammad Abdullah <ma22052000@gmail.com>
2023-09-19 21:23:33 +0700

    tambah route login dan register

--- routes/api.go
+++ routes/api.go
@@ -7,9 +7,15 @@ import (
 )
 
 func SetupRoutes(router *gin.Engine) {
+	router.POST("/api/register", controllers.Register)
+    router.POST("/api/login", controllers.Login)
+
+	//get user
 	router.GET("/api/buku", controllers.IndexBuku)
 	router.POST("/api/buku", controllers.StoreBuku)
 	router.GET("/api/buku/:id", controllers.ShowBuku)
 	router.PUT("/api/buku/:id", controllers.UpdateBuku)
 	router.DELETE("/api/buku/:id", controllers.DestroyBuku)
+
+	//logout
 }
