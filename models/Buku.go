f25446aff8a4c9dd4e8fd2d90271ec3f4b51a5df
Muhammad Abdullah <ma22052000@gmail.com>
2023-09-19 21:34:56 +0700

    perapian kode

--- models/Buku.go
+++ models/Buku.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Bukus struct {
-    ID     uint   `json:"id" gorm:"primary_key"`
-    Judul  string `json:"judul"`
+    ID uint   `json:"id" gorm:"primary_key"`
+    Judul string `json:"judul"`
     Tahun string `json:"tahun"`
     UserID uint `json:"user_id"`
-	CreatedAt time.Time  `gorm:"autoCreateTime"`
-    UpdatedAt time.Time  `gorm:"autoUpdateTime"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+    UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
--- models/User.go
+++ models/User.go
@@ -5,9 +5,10 @@ import (
 )
 
 type User struct {
-	Name  string `json:"name"`
-    Email  string `json:"email" gorm:"unique;not null"`
-    Password  string `json:"password" gorm:"not null"`
-	CreatedAt time.Time  `gorm:"autoCreateTime"`
-    UpdatedAt time.Time  `gorm:"autoUpdateTime"`
+    ID uint `json:"id" gorm:"primary_key"`
+	Name string `json:"name"`
+    Email string `json:"email" gorm:"unique;not null"`
+    Password string `json:"password" gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+    UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
