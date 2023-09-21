b232f62e13a3863a9632c121479f11e0cbe17a1e
Muhammad Abdullah <ma22052000@gmail.com>
2023-09-21 10:50:45 +0700

    tambah fungsi : generate token dan auth

--- controllers/AuthController.go
+++ controllers/AuthController.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+    "fmt"
+    "time"
     "net/http"
     "github.com/gin-gonic/gin"
     "golang.org/x/crypto/bcrypt"
+    "github.com/dgrijalva/jwt-go"
 
 	"buku-api-gin/config"
 	"buku-api-gin/models"
@@ -74,10 +77,63 @@ func Login(c *gin.Context) {
         return
     }
 
+    token, err := GenerateJWTToken(input.Email)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+        return
+    }
+
     login := gin.H{
         "_pesan": "Berhasil Login",
         "data": user,
+        "token" : token,
     }
 
     c.JSON(http.StatusCreated, login)
+}
+
+//Authorization logic
+var (
+	secretKey = []byte("ini_kunci")
+)
+
+func GenerateJWTToken(username string) (string, error) {
+	// Buat token dengan waktu kadaluwarsa 1 jam (sesuaikan sesuai kebutuhan Anda)
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = username
+	claims["exp"] = time.Now().Add(time.Minute * 5).Unix()
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+        if tokenString == "" {
+			// Header "Authorization" kosong, Anda dapat mengembalikan token kosong atau respons Unauthorized.
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token not Found"})
+			c.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return secretKey, nil
+		})
+
+		if !token.Valid {
+			fmt.Print("error : ", err)
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
 }
